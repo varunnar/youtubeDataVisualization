69451eed1eb166fc7f5275fe874f336eb169cc68
Varun Narayanswamy <varun@Varuns-MacBook-Pro-3.local>
2025-02-06 14:50:18 -0700

    setting up .gitignore

---
+++ src/openAI/test.mjs
@@ -0,0 +1,16 @@
+import OpenAI from "openai";
+const openai = new OpenAI();
+
+const completion = await openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [
+        { role: "system", content: "You are a helpful assistant." },
+        {
+            role: "user",
+            content: "Write a haiku about recursion in programming.",
+        },
+    ],
+    store: true,
+});
+
+console.log(completion.choices[0].message);
